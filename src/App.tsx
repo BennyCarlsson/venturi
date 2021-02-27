8ca950e9aa4f63e9da10551e5afce1ad20916350
Benny Carlsson <carlsson.benny93@gmail.com>
2021-02-27 17:14:04 +0100

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import StartPage from './src/views/StartPage';
+import Favorites from './src/views/Favorite';
+import Destination from './src/views/Destination';
+import NotFound from './src/views/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <StartPage />
+          </Route>
+          <Route path="/favorites">
+            <Favorites/>
+          </Route>
+          <Route path="/destination">
+            <Destination />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/favorites">About</Link>
+            </li>
+            <li>
+              <Link to="/destination">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
     </div>
   );
 }
