3dcaf0bcfeb531c44edb4cf10e775746072fb237
Benny Carlsson <carlsson.benny93@gmail.com>
2021-02-28 18:05:17 +0100

    chore: add styled components

--- src/views/StartPage/index.tsx
+++ src/views/StartPage/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Heading} from './styles'
 
 function StartPage() {
   return (
     <div>
-      StartPage
+      <Heading>StartPage</Heading>
     </div>
   );
 }
