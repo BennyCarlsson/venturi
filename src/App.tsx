84bc0d60183d639312b3bf56393c2e27e7f0c201
Benny Carlsson <carlsson.benny93@gmail.com>
2021-03-01 20:35:35 +0100

    fix: /venturi path for test site

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Favorites from 'views/Favorite';
 import Destination from 'views/Destination';
@@ -16,37 +11,40 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-            <StartPage />
-          </Route>
-          <Route path="/favorites">
-            <Favorites/>
-          </Route>
-          <Route path="/destination">
-            <Destination />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <StartPage />
+            </Route>
+            <Route path="/venturi">
+              <StartPage />
+            </Route>
+            <Route path="/favorites">
+              <Favorites />
+            </Route>
+            <Route path="/destination">
+              <Destination />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
 
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/favorites">About</Link>
-            </li>
-            <li>
-              <Link to="/destination">Users</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/favorites">About</Link>
+              </li>
+              <li>
+                <Link to="/destination">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Router>
     </div>
   );
 }
