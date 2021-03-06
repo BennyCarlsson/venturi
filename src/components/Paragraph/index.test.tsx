be2d2e22b1981226359b5b6e5cf46bf860baa81d
Benny Carlsson <carlsson.benny93@gmail.com>
2021-03-06 20:21:36 +0100

    test: Paragraph

--- src/components/Paragraph/index.test.tsx
+++ src/components/Paragraph/index.test.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import Paragraph from '.';
+import Paragraph, { fontWeight } from '.';
 
 describe('<Paragraph />', () => {
   it('should match snapshot', () => {
-    const tree = renderer.create(<Paragraph title="test" />).toJSON();
+    const tree = renderer.create(<Paragraph>test</Paragraph>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('should renders without crashing', () => {
-    render(<Paragraph title="test" />);
+    render(
+      <Paragraph fontSize={16} fontWeight={fontWeight.bold} color="green" center>
+        test
+      </Paragraph>
+    );
     expect(screen).toBeTruthy();
+  });
+
+  it('should renders text', () => {
+    render(<Paragraph>asd 123</Paragraph>);
+    expect(screen.queryByText('asd 123')).toBeInTheDocument();
   });
 });
