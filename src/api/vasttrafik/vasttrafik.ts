9b5dc20df2854e6d01be4801c20c07624448b182
Benny Carlsson <carlsson.benny93@gmail.com>
2021-03-07 18:55:07 +0100

    refactor: api

--- src/api/vasttrafik/vasttrafik.ts
+++ src/api/vasttrafik/vasttrafik.ts
@@ -1,18 +1,25 @@
 import { ApiError, DepartureBoard, Token } from 'types';
-import { getTime, getExpireTime, isTokenValid } from './helpers';
+import { getExpireTime, isTokenValid } from './helpers';
+import { fetchAccessToken, fetchDepartureBoard } from './vasttrafikApi';
 
-const id = 'T0hDc2txdEp6cDhMYlVmRjhsaWVfeERHdDBBYTp6ME5wWWY3dlYxbUFhTEVFbkdyRFprTGEwODBh';
 let token: Token = { accessToken: '', expires: null };
+
+const updateToken = async () => {
+  const data = await fetchAccessToken();
+  const expires = getExpireTime(data.expires_in);
+  const accessToken = data.access_token;
+  token = { accessToken, expires };
+};
 
 export const getDepartureBoard = async (fromStopId: string) => {
   let data;
   if (!isTokenValid(token)) {
     await updateToken();
   }
-  let response = await fetchDepartureBoard(fromStopId);
+  let response = await fetchDepartureBoard(fromStopId, token);
   if (response.status === 401) {
     await updateToken();
-    response = await fetchDepartureBoard(fromStopId);
+    response = await fetchDepartureBoard(fromStopId, token);
   }
   if (response.status === 200) {
     data = await response.json();
@@ -20,36 +27,4 @@ export const getDepartureBoard = async (fromStopId: string) => {
   }
   data = await response.json();
   return data as ApiError;
-};
-
-const fetchDepartureBoard = async (fromStopId: string): Promise<Response> => {
-  const { date, time } = getTime();
-  const tripUrl = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=${fromStopId}&date=${date}&time=${time}&format=json`;
-  const response = await fetch(tripUrl, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token.accessToken}`
-    }
-  });
-  return response;
-};
-
-const fetchAccessToken = async () => {
-  const response = await fetch('https://api.vasttrafik.se:443/token', {
-    headers: {
-      Authorization: `Basic ${id}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    method: 'POST',
-    body: 'grant_type=client_credentials'
-  });
-  const data = await response.json();
-  return data;
-};
-
-const updateToken = async () => {
-  const data = await fetchAccessToken();
-  const expires = getExpireTime(data.expires_in);
-  const accessToken = data.access_token;
-  token = { accessToken, expires };
 };
---
+++ src/api/vasttrafik/vasttrafikApi.ts
@@ -0,0 +1,29 @@
+import { Token } from 'types';
+import { getTime } from './helpers';
+
+const id = 'T0hDc2txdEp6cDhMYlVmRjhsaWVfeERHdDBBYTp6ME5wWWY3dlYxbUFhTEVFbkdyRFprTGEwODBh';
+
+export const fetchDepartureBoard = async (fromStopId: string, token: Token): Promise<Response> => {
+  const { date, time } = getTime();
+  const tripUrl = `https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=${fromStopId}&date=${date}&time=${time}&format=json`;
+  const response = await fetch(tripUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token.accessToken}`
+    }
+  });
+  return response;
+};
+
+export const fetchAccessToken = async () => {
+  const response = await fetch('https://api.vasttrafik.se:443/token', {
+    headers: {
+      Authorization: `Basic ${id}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: 'POST',
+    body: 'grant_type=client_credentials'
+  });
+  const data = await response.json();
+  return data;
+};
