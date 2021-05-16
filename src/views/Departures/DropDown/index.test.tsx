e09e5cb1657bce33288b5be91660c2eb68f2d0ef
Benny Carlsson <carlsson.benny93@gmail.com>
2021-05-16 18:13:33 +0200

    test: unit tests for SearchInput

--- src/views/Departures/DropDown/index.test.tsx
+++ src/views/Departures/DropDown/index.test.tsx
@@ -1,15 +1,55 @@
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import { LocationType } from 'redux/locationsSlice';
 import DropDown from '.';
 
 describe('<DropDown />', () => {
+  beforeEach(() => {
+    render(
+      <DropDown
+        locations={[
+          {
+            id: '1',
+            idx: '1',
+            name: 'name',
+            lat: '1',
+            lon: '1',
+            type: LocationType.CoordLocation
+          }
+        ]}
+        setInput={jest.fn()}
+        setLocation={jest.fn()}
+        show
+      />
+    );
+  });
+
   it('should match snapshot', () => {
-    const tree = renderer.create(<DropDown title="test" />).toJSON();
+    const tree = renderer
+      .create(
+        <DropDown
+          locations={[
+            {
+              id: '1',
+              idx: '1',
+              name: 'name',
+              lat: '1',
+              lon: '1',
+              type: LocationType.CoordLocation
+            }
+          ]}
+          setInput={jest.fn()}
+          setLocation={jest.fn()}
+          show
+        />
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('should render without crashing', () => {
-    render(<DropDown title="test" />);
     expect(screen).toBeTruthy();
   });
+
+  //Todo: more tests
 });
--- src/views/Departures/SearchInput/index.test.tsx
+++ src/views/Departures/SearchInput/index.test.tsx
@@ -1,15 +1,64 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderer from 'react-test-renderer';
 import SearchInput from '.';
 
 describe('<SearchInput />', () => {
+  const value = 'texttext';
+  const onChange = jest.fn();
+  const setShowDropDown = jest.fn();
+
+  beforeEach(() => {
+    render(<SearchInput value={value} onChange={onChange} setShowDropDown={setShowDropDown} />);
+  });
+
   it('should match snapshot', () => {
-    const tree = renderer.create(<SearchInput title="test" />).toJSON();
+    const tree = renderer.create(
+      <SearchInput value="123" onChange={jest.fn()} setShowDropDown={jest.fn()} />
+    );
     expect(tree).toMatchSnapshot();
   });
 
   it('should render without crashing', () => {
-    render(<SearchInput title="test" />);
     expect(screen).toBeTruthy();
+  });
+
+  it('should have value as input value', () => {
+    expect(screen.getByRole('textbox')).toHaveValue(value);
+  });
+
+  it('should call onChange onChange', () => {
+    userEvent.type(screen.getByRole('textbox'), 'Hello');
+    expect(onChange).toHaveBeenCalledTimes(5);
+  });
+
+  it('should call setShowDropDown onFocus', () => {
+    screen.getByRole('textbox').focus();
+    expect(setShowDropDown).toHaveBeenCalledTimes(1);
+    expect(setShowDropDown).toHaveBeenCalledWith(true);
+  });
+
+  it('should not call setShowDropDown onFocus when value is none', () => {
+    cleanup();
+    render(<SearchInput value={''} onChange={onChange} setShowDropDown={setShowDropDown} />);
+    screen.getByRole('textbox').focus();
+    expect(setShowDropDown).not.toHaveBeenCalled();
+  });
+
+  it('should not call setShowDropDown onFocus with value.length is 1', () => {
+    cleanup();
+    render(<SearchInput value={'a'} onChange={onChange} setShowDropDown={setShowDropDown} />);
+    screen.getByRole('textbox').focus();
+    expect(setShowDropDown).not.toHaveBeenCalled();
+  });
+
+  it('should call setShowDropDown onBlur', async () => {
+    screen.getByRole('textbox').focus();
+    screen.getByRole('textbox').blur();
+
+    expect(setShowDropDown).toHaveBeenCalledTimes(1); // from onFocus
+
+    await waitFor(() => expect(setShowDropDown).toHaveBeenCalledTimes(2)); // from onBlur
+    await waitFor(() => expect(setShowDropDown).toHaveBeenNthCalledWith(2, false));
   });
 });
