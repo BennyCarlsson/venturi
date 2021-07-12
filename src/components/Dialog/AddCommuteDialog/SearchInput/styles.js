f2aff2d4707f3c278dd642aa87cb153bd631da11
Benny Carlsson <benny.carlsson@humblebee.se>
2021-07-12 11:23:42 +0200

    fix: border radius on input

--- src/components/Dialog/AddCommuteDialog/SearchInput/styles.js
+++ src/components/Dialog/AddCommuteDialog/SearchInput/styles.js
@@ -6,6 +6,7 @@ export const Input = styled.input`
   width: 100%;
   height: 36px;
   border: none;
+  border-radius: 0;
   border-bottom: 1px solid #726a95;
   font-family: 'Montserrat';
   font-style: normal;
